b3993ac6cb50bd7a492be960fc164fcfa028cf45
LaNa-17 <atomx361@gmail.com>
2024-03-12 03:39:35 +0700

    crud

--- src/main/java/com/spring_crud/controller/UserController.java
+++ src/main/java/com/spring_crud/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.spring_crud.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import com.spring_crud.model.entity.User;
 import com.spring_crud.services.UserService;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,8 +16,23 @@ public class UserController {
 
     private final UserService userService;
 
+    @PostMapping("/api/user")
+    public User create(@RequestBody User user) {
+        return userService.save(user);
+    }
+
     @GetMapping("/api/user")
     public Iterable<User> findAll() {
         return userService.findAll();
+    }
+
+    @GetMapping("/api/user/{id}")
+    public User getById(@PathVariable("id") Long id) {
+        return userService.getById(id);
+    }
+
+    @PutMapping("/api/user")
+    public User update(@RequestBody User user) {
+        return userService.save(user);
     }
 }
--- src/main/java/com/spring_crud/services/UserService.java
+++ src/main/java/com/spring_crud/services/UserService.java
@@ -1,5 +1,7 @@
 package com.spring_crud.services;
 
+import java.util.Optional;
+
 // import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +15,28 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    @SuppressWarnings("null")
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
     public Iterable<User> findAll() {
         return userRepository.findAll();
+    }
+
+    // @SuppressWarnings( "null" })
+    @SuppressWarnings("null")
+    public User getById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (!user.isPresent()) {
+            return null;
+        }
+        return user.get();
+    }
+
+    @SuppressWarnings("null")
+    public void delete(Long id) {
+        userRepository.deleteById(id);
     }
 }
